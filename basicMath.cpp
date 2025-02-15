a3019d31a8bdb21979d7f2602ed8e43780b752df
Joanna Keza <joanna.keza@stu.ocsb.ca>
2025-02-15 15:37:57 +0000

    Added

---
+++ basicMath.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+// Copyright (c) 2025 Joanna Keza All rights reserved.
+//
+// Created by: Joanna Keza
+// Date: February 14, 2025
+// This program does basic math.
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+    std::cout << "9+2= " << 9 + 2 << std::endl;
+    std::cout << "10-5= " << 10 - 5 << std::endl;
+    std::cout << "100*10= " << 100 * 10 << std::endl;
+    std::cout << "10/144= " << float(10) / float(144) << std::endl;
+    std::cout << "3**10= " << pow(3, 10) << std::endl;
+    std::cout << "81**0.5= " << pow(81, 0.5) << std::endl;
+}
